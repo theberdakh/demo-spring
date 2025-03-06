af2b03c69b53476e290d1263e32d4f19e0465f58
theberdakh <theberdakh@proton.me>
2025-03-06 22:22:28 +0500

    init

---
+++ src/main/kotlin/com/theberdakh/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.theberdakh.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
